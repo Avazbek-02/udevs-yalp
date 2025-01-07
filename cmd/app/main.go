d95772bb9562cc09260a11f39f7d71586c81c86b
Avazbek-02 <avazbekmambetov9@gmail.com>
2025-01-07 11:07:18 +0500

    test

---
+++ cmd/app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Avazbek-02/udevslab-lesson6/config"
+	"github.com/Avazbek-02/udevslab-lesson6/internal/app"
+)
+
+func main() {
+	// Configuration
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("Config error: %s", err)
+	}
+
+	// Run
+	app.Run(cfg)
+}
