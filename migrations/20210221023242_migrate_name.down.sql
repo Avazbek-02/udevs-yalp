bbc360996646d6f8d7b8d37364c9259e863e7de5
Avazbek-02 <avazbekmambetov9@gmail.com>
2025-01-07 14:48:50 +0500

    docker is done

---
+++ migrations/20210221023242_migrate_name.down.sql
@@ -0,0 +1,8 @@
+drop table if exists users;
+drop table if exists sessions;
+
+drop type platform;
+drop type user_status;
+drop type gender;
+drop type user_role;
+drop type user_type;
--- migrations/20210221023242_migrate_name.up.sql
+++ migrations/20210221023242_migrate_name.up.sql
@@ -1,110 +1,52 @@
-CREATE TYPE auth_user_type AS ENUM (
-  'client',
+CREATE TYPE "user_type" AS ENUM (
+  'user',
+  'admin'
+);
+
+CREATE TYPE "user_role" AS ENUM (
+  'user',
   'admin',
   'superadmin'
 );
 
-CREATE TYPE auth_status AS ENUM (
+CREATE TYPE "gender" AS ENUM (
+  'male',
+  'female'
+);
+
+CREATE TYPE "user_status" AS ENUM (
   'active',
   'blocked',
-  'in_verify'
+  'inverify'
 );
 
-CREATE TYPE project_status AS ENUM (
-  'new',
-  'in_verify',
-  'verified',
-  'canceled',
-  'won',
-  'lost'
+CREATE TYPE "platform" AS ENUM (
+  'admin',
+  'web',
+  'mobile'
 );
 
-CREATE TABLE auth_user (
-  id uuid PRIMARY KEY,
-  full_name varchar NOT NULL,
-  phone_number varchar(10) NOT NULL DEFAULT '',
-  username varchar(20) NOT NULL DEFAULT '',
-  password varchar,
-  user_type auth_user_type NOT NULL DEFAULT 'client',
-  status auth_status NOT NULL DEFAULT 'in_verify',
-  created_at timestamp NOT NULL DEFAULT 'now()',
-  updated_at timestamp NOT NULL DEFAULT 'now()'
+CREATE TABLE if not exists "users" (
+  "id" UUID UNIQUE PRIMARY KEY NOT NULL,
+  "user_type" user_type NOT NULL,
+  "user_role" user_role NOT NULL,
+  "email" VARCHAR(255) UNIQUE NOT NULL,
+  "password_hash" VARCHAR(255) NOT NULL,
+  "name" VARCHAR(255),
+  "gender" gender NOT NULL DEFAULT 'male',
+  "profile_picture" VARCHAR(255),
+  "bio" TEXT,
+  "status" user_status NOT NULL DEFAULT 'inverify',
+  "created_at" TIMESTAMP DEFAULT (CURRENT_TIMESTAMP),
+  "updated_at" TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
 );
 
-CREATE UNIQUE INDEX ON auth_user (phone_number, username);
-
-CREATE TABLE auth_session (
-  id uuid PRIMARY KEY,
-  user_id uuid NOT NULL REFERENCES auth_user (id),
-  ip_address varchar NOT NULL,
-  user_agent text NOT NULL DEFAULT '',
-  is_active bool NOT NULL DEFAULT 'true',
-  expires_at timestamp,
-  last_active_at timestamp NOT NULL DEFAULT 'now()',
-  created_at timestamp NOT NULL DEFAULT 'now()',
-  updated_at timestamp NOT NULL DEFAULT 'now()'
+CREATE TABLE if not exists "sessions" (
+  "id" UUID UNIQUE PRIMARY KEY NOT NULL,
+  "user_id" UUID NOT NULL REFERENCES Users(id) ON delete cascade,
+  "user_agent" TEXT NOT NULL,
+  "platform" ENUM(admin,web,mobile) NOT NULL,
+  "ip_address" VARCHAR(64) NOT NULL,
+  "created_at" TIMESTAMP DEFAULT (CURRENT_TIMESTAMP),
+  "updated_at" TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
 );
-
-CREATE TABLE cycle (
-  id uuid PRIMARY KEY,
-  title varchar NOT NULL,
-  description text NOT NULL,
-  start_date timestamp NOT NULL,
-  project_accept_end_date timestamp NOT NULL CHECK (project_accept_end_date > start_date),
-  moderation_end_date timestamp NOT NULL CHECK (moderation_end_date > project_accept_end_date),
-  voting_end_date timestamp NOT NULL CHECK (voting_end_date > moderation_end_date),
-  created_at timestamp NOT NULL DEFAULT 'now()',
-  updated_at timestamp NOT NULL DEFAULT 'now()'
-);
-
-CREATE TABLE region (
-  id uuid PRIMARY KEY,
-  title varchar NOT NULL,
-  created_at timestamp NOT NULL DEFAULT 'now()',
-  updated_at timestamp NOT NULL DEFAULT 'now()'
-);
-
-CREATE TABLE district (
-  id uuid PRIMARY KEY,
-  region_id uuid NOT NULL REFERENCES region (id),
-  title varchar NOT NULL,
-  created_at timestamp NOT NULL DEFAULT 'now()',
-  updated_at timestamp NOT NULL DEFAULT 'now()'
-);
-
-CREATE TABLE project_type (
-  id uuid PRIMARY KEY,
-  title varchar NOT NULL,
-  slug varchar UNIQUE NOT NULL,
-  active bool NOT NULL DEFAULT 'true',
-  created_at timestamp NOT NULL DEFAULT 'now()',
-  updated_at timestamp NOT NULL DEFAULT 'now()'
-);
-
-CREATE TABLE project (
-  id uuid PRIMARY KEY,
-  title varchar NOT NULL,
-  description varchar NOT NULL,
-  images varchar[] NOT NULL DEFAULT '{}',
-  district_id uuid NOT NULL REFERENCES district (id),
-  type_id uuid NOT NULL REFERENCES project_type (id),
-  creater_id uuid NOT NULL REFERENCES auth_user (id),
-  cycle_id uuid NOT NULL REFERENCES cycle (id),
-  status project_status NOT NULL DEFAULT 'new',
-  created_at timestamp NOT NULL DEFAULT 'now()',
-  updated_at timestamp NOT NULL DEFAULT 'now()'
-);
-
-CREATE TABLE vote (
-    id uuid PRIMARY KEY,
-    creater_id uuid NOT NULL REFERENCES auth_user (id),
-    project_id uuid NOT NULL REFERENCES project (id),
-    cycle_id uuid NOT NULL REFERENCES cycle (id),
-    phone_number varchar NOT NULL,
-    passport_series varchar NOT NULL,
-    created_at timestamp NOT NULL DEFAULT 'now()',
-    updated_at timestamp NOT NULL DEFAULT 'now()'
-);
-
-CREATE UNIQUE INDEX ON vote (cycle_id, phone_number);
-CREATE UNIQUE INDEX ON vote (cycle_id, passport_series);
